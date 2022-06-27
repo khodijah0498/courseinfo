f08c84ba7518f5d98bbefb07803b3a7cd9318b5c
lawal khodijat <khodijahlawal@gmail.com>
2022-06-27 16:52:43 +0100

    project init

--- src/App.js
+++ src/App.js
@@ -42,35 +42,37 @@ return(
 
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
   };
   return (
     <main>
-      <Header course={course} />
-      
-      <Content part1={part1.name} exercises1={part1.exercises} />
-      <Content part2={part2.name} exercises2={part2.exercises} />
-      <Content part3={part3.name} exercises3={part3.exercises} />
+      <Header course={course.name} />
+
+      <Content part1={course.parts[0].name} exercises1={course.parts[0].exercises} />
+      <Content part2={course.parts[1].name} exercises2={course.parts[1].exercises} />
+      <Content part3={course.parts[2].name} exercises3={course.parts[2].exercises} />
       <Total
-        exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises}
+        exercises1={course.parts[0].exercises}
+        exercises2={course.parts[1].exercises}
+        exercises3={course.parts[2].exercises}
       />
     </main>
   );
-  
-  
 };
 
 export default App;
